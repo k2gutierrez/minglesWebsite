282ed44654997d44580aea2ab6d49922c16f93fd
k2gutierrez <cchimal.gutierrez@gmail.com>
2025-05-28 12:06:08 -0600

    changed the curtis addresses

--- src/components/engine/CONSTANTS.ts
+++ src/components/engine/CONSTANTS.ts
@@ -10,6 +10,8 @@ export const CavaStakeAddress = "0x8FD1942b961ed13aC50a5873bA06fABE37168f6D"
 //Curtis
 export const MinglesCurtis = "0x15f4e543F6869e43baDF3C6365D7C759c1B397D8"
 
-export const CavaNFTAddressCurtis = "0x1965131c250d128a2336F85bC7DEca803762770f"
+export const CavaNFTAddressCurtis = "0xE56Fd0FF0B597AD16E97cf3D2f33B9C7DCE31e04"
 
-export const CavaStakeAddressCurtis = "0x6F52CfBc3596BEA53111017A6F1F1C5604CB7A12"
+export const CavaStakeAddressCurtis = "0xf3f05DC8A26FDd87c750c97FCbb0e2fa9dbda33E"
+
+// old staking contract address = 0x6F52CfBc3596BEA53111017A6F1F1C5604CB7A12
