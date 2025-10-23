3c6e347b9e9707b659a08942873a7b8488c96042
k2gutierrez <cchimal.gutierrez@gmail.com>
2025-10-22 21:07:45 -0600

    stickers presale

--- src/app/stickers/page.tsx
+++ src/app/stickers/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import worm from "../../../public/images/White_Hearts_Right-p-500.png";
+//import worm from "../../../public/images/White_Hearts_Right-p-500.png";
 import { useRouter } from "next/navigation";
 import vom from "../../../public/images/vomit.gif";
 
@@ -15,12 +15,14 @@ export default function MintBrowserPage() {
 
     return (
 
-        <main className="font-[family-name:var(--font-hogfish)] px-5 text-center" style={{ minHeight: "100vh", display: "flex", flexDirection: "column", alignItems: "center", gap: 16, justifyContent: "center" }}>
-            <p className="text-black" style={{ fontSize: 22, fontWeight: 800 }}>Mingles Tequila — At TON</p>
+        <main className="px-5 text-center" style={{ minHeight: "100vh", display: "flex", flexDirection: "column", alignItems: "center", gap: 16, justifyContent: "center" }}>
+            <p className="font-[family-name:var(--font-hogfish)] text-black" style={{ fontSize: 28, fontWeight: 800 }}>PRE-SALE</p>
+            <p className="font-[family-name:var(--font-hogfish)] text-black" style={{ fontSize: 20, fontWeight: 800 }}>Limited Sticker Collection</p>
+            <p className="font-[family-name:var(--font-hogfish)] text-black" style={{ fontSize: 20, fontWeight: 800 }}>Mingles Tequila</p>
             <Image src={vom} alt="Mingles-TON" width={250} height={250} />
-            <p className="text-black" style={{ opacity: 0.7, fontSize: 13 }}>Register on the Mingle Stickers PRE-SALE. Send 20 APE to the Address at the FORM and fill the information so we can Airdrop soon.</p>
-            <p className="text-black" style={{ opacity: 0.7, fontSize: 13 }}>Cheers!</p>
-            <button className="bg-red-500" onClick={form}  style={{ padding: 12, borderRadius: 12 }}>
+            <p className="font-[family-name:var(--font-hogfish)] text-black" style={{ opacity: 0.7, fontSize: 13 }}>We are Dropping TON stickers. Send 20 APE to this wallet <span className="font-[family-name:var(--font-pressura)]" style={{ opacity: 1, fontSize: 15 }}> 0xA3E187116bcAD707a6b12C7DeC32037a8Be45b1d </span> and register on the form below.</p>
+            <p className="font-[family-name:var(--font-hogfish)] text-black" style={{ opacity: 0.7, fontSize: 13 }}>Cheers!</p>
+            <button className="font-[family-name:var(--font-hogfish)] bg-red-500" onClick={form}  style={{ padding: 12, borderRadius: 12 }}>
                 Register for PRE-SALE
             </button>
         </main>
